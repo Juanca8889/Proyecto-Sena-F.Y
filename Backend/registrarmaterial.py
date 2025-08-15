941a9e419377b0de2122097360fb79f9651ef26b
Andres <ac1859420@gmail.com>
2025-08-14 20:39:04 -0500

    Funcion registro de material

--- Backend/registrarmaterial.py
+++ Backend/registrarmaterial.py
@@ -11,35 +11,19 @@ class Inventario:
             "fecha": fecha
         }
         self.materiales.append(material)
-        print(f"material '{nombre}' registrado con exito.")
-
-    def pedir_material(self):
-        while True:
-            nombre = input("ingrese el nombre del material: ")
-            
-            while True:
-                try:
-                    cantidad = int(input("ingrese la cantidad: "))
-                    break
-                except ValueError:
-                    print("ingrese un numero valido.")
-            
-            tipo = input("ingrese el tipo de material: ")
-            proveedor = input("ingrese el proveedor: ")
-            fecha = input("ingrese la fecha de ingreso (DD/MM/AAAA): ")
-            
-            self.registrar_material(nombre, cantidad, tipo, proveedor, fecha)
-
-            continuar = input("registrar otro material? (s/n): ").lower()
-            if continuar != "s":
-                break
-
+        
     def mostrar_materiales(self):
-        print("Lista de materiales registrados:")
-        for m in self.materiales:
-            print(m)
+        return self.materiales
 
 
+# Ejemplo
 inventario = Inventario()
-inventario.pedir_material()
-inventario.mostrar_materiales()
+inventario.registrar_material("Parche", 100, "Caucho", "Proveedor A", "14/08/2025")
+inventario.registrar_material("Neumatico", 10, "Cp661 165/70r13", "Proveedor B", "15/08/2025")
+
+materiales_guardados =inventario.mostrar_materiales()
+print("Lista de materiales registrados:")
+for material in materiales_guardados:
+    print(material)
+
+
