fa950d5c6fc51ed32a6907e80168fbdfee6be703
Andres <ac1859420@gmail.com>
2025-08-25 19:47:05 -0500

    servicios v1

---
+++ Backend/ordenesdeservicio.py
@@ -0,0 +1,24 @@
+import mysql.connector
+class Servicios :
+    def __init__(self):
+        self.conexion = mysql.connector.connect(
+            host="localhost",
+            user="root",        # cambia por tu usuario
+            password="",        # cambia por tu contraseña
+            database="montallantasfy"
+        )
+        self.cursor = self.conexion.cursor(dictionary=True)
+    
+    def registrarorden(self,descripcion,tipo,fecha,cliente,usuario):
+        query = """
+        INSERT INTO Servicios (descripcion,tipo,fecha,cliente,usuario)
+        VALUES (%s, %s, %s, %s, %s)
+        """
+        valores = (descripcion,tipo,fecha,cliente,usuario)
+        self.cursor.execute(query, valores)
+        self.conexion.commit()
+        print("✅ Servicio registrado con éxito.")
+
+serv = Servicios ()
+
+serv.registrarorden("cambio de aceite","taller","2025-01-03","cliente1","usuario1")  
