5c2df0392e0881c59902de7722456bb119b43d79
Juan Camilo <sierra9camilo9@gmail.com>
2025-09-04 21:45:08 -0500

    organizacion de archivos y funciones de botones full py

--- frontend/App.py
+++ frontend/App.py
@@ -1,18 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import sys
 import os
-
-
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+#Imports de base de datso y backend
 from BD.conexion import  ConexionUsuario, verificar_usuario
-from BD.Clientes import  ConexionClientes
+from Backend.Clientes import  ConexionClientes
 
 
 app = Flask(__name__)
 app.secret_key = 'wjson'
 
-
+#login y registro
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -33,16 +32,8 @@ def login():
         else:
             return "Usuario o contraseña incorrectos"
 
-    return print("")
+    return render_template('login.html')
 
-
-@app.route('/Admin')
-def home():
-    return render_template('index.html')
-
-@app.route('/Empleado')
-def Empleado():
-    return render_template('empleado.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -64,18 +55,53 @@ def register():
 
     return render_template('register.html')
 
+#rutas de admin y empleado vistas de los menu inicialmente
 
+@app.route('/Admin')
+def home():
+    return render_template('index.html')
 
+@app.route('/Empleado')
+def Empleado():
+    return render_template('empleado.html')
+
+#rutas de gestion de clientes
 
 @app.route("/clientes")
 def mostrar_clientes():
+    orden = request.args.get("orden", None)  
+    
     conexion = ConexionClientes()
-    clientes = conexion.mostrar_clientes()
+    clientes = conexion.mostrar_clientes(orden)
     conexion.cerrar()
 
     return render_template("Gestion_clientes.html", usuarios=clientes)
 
 
+@app.route("/eliminar_cliente/<int:id_cliente>", methods=["POST"])
+def eliminar_cliente(id_cliente):
+    conexion = ConexionClientes()
+    conexion.cerrar()
+    return redirect(url_for("mostrar_clientes"))
+
+@app.route("/editar_cliente/<int:id_cliente>", methods=["GET", "POST"])
+def editar_cliente(id_cliente):
+    conexion = ConexionClientes()
+    if request.method == "POST":
+        nombre = request.form["nombre"]
+        correo = request.form["correo"]
+        celular = request.form["celular"]
+        conexion.actualizar_usuario(id_cliente, nombre, correo, celular)
+        conexion.cerrar()
+        return redirect(url_for("mostrar_clientes"))
+    else:
+        cliente = conexion.obtener_usuario(id_cliente)
+        conexion.cerrar()
+        return render_template("editar_cliente.html", cliente=cliente)
+
+
+
+#para correr la app
 
 if __name__ == '__main__':
     app.run(debug=True)
