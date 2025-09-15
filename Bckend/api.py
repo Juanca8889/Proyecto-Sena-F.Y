727157142e387a2b6fb7a73d355276b0698272c0
Laura <isabellamahecha31@gmail.com>
2025-09-15 06:53:04 -0500

    api

--- Bckend/api.py
+++ Bckend/api.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from conexion_detalle_compra import ConexionDetalleCompra  
+from conexion_detalle_compra import ConexionDetalleCompraaa  
 
 app = Flask(__name__)
 CORS(app)
